5f3424e8f98f9e872f3c7ee507e46252254f6aef
Seif <britzgrank1@users.noreply.github.com>
2026-05-11 13:47:48 +0200

    variables-getter

--- src/main/java/app/entities/User.java
+++ src/main/java/app/entities/User.java
@@ -4,20 +4,29 @@ public class User {
     private int userId;
     private String email;
     private String password;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private int phoneNumber;
+    private int postalCode;
     private double balance;
 
-    public User(int userId, String email, String password, double balance) {
+    public User(int userId, String email, String password, String firstName, String lastName,
+                String address, int phoneNumber, int postalCode, double balance) {
         this.userId = userId;
         this.email = email;
         this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.postalCode = postalCode;
         this.balance = balance;
-
     }
 
     public int getUserId() {
         return userId;
     }
-
 
     public String getEmail() {
         return email;
@@ -27,8 +36,31 @@ public class User {
         return password;
     }
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public int getPostalCode() {
+        return postalCode;
+    }
+
     public double getBalance() {
         return balance;
     }
-
 }
+
+
+
+
