3184150787d5acbf5aeb25f8d75f46d125a8951b
Emil <emil234@hotmail.dk>
2026-05-12 09:41:56 +0200

    entitet klasser sat op og mapper klasser oprettet (database ikke udfyldt endnu derfor er mapper klasser tom)

--- src/main/java/app/entities/User.java
+++ src/main/java/app/entities/User.java
@@ -1,4 +1,90 @@
 package app.entities;
 
 public class User {
+    private int id;
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private String address;
+    private int zipcode;
+
+    public User(int id, String email, String password, String firstName, String lastName, String phone, String address, int zipcode) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+        this.address = address;
+        this.zipcode = zipcode;
+        }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(int zipcode) {
+        this.zipcode = zipcode;
+    }
 }
+
+
+
