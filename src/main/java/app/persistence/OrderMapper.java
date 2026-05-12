6802e6d70eead66fd0b208f86aa874aadd18bd12
Seif <britzgrank1@users.noreply.github.com>
2026-05-12 10:51:58 +0200

    ordermapper-part1

--- src/main/java/app/persistence/OrderMapper.java
+++ src/main/java/app/persistence/OrderMapper.java
@@ -1,4 +1,40 @@
 package app.persistence;
 
+import app.exceptions.DatabaseException;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class OrderMapper {
+
+    public static List<Object[]> getOrdersByUser(int userId, ConnectionPool connectionPool) throws DatabaseException {
+        List<Object[]> orders = new ArrayList<>();
+        String sql = """
+                SELECT order_id, total_price,
+                FROM orders
+                WHERE customer_id = ?
+                ORDER BY order_id DESC
+                """;
+        try (Connection conn = connectionPool.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setInt(1, userId);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                orders.add(new Object[]{
+                        rs.getInt("order_id"),
+                        rs.getDouble("tota_price"),
+                        rs.getString("")
+                });
+            }
+        } catch (SQLException e) {
+            throw new DatabaseException("Kunne ikke hente ordrer", e);
+        }
+        return orders;
+    }
 }
